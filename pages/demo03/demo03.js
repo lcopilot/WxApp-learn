48c6790cdd54cd5b0256ea2a4870b3ef16a01591
MuGe <MuGe@qq.com>
2020-02-18 09:51:25 +0800

    组件学习

--- pages/demo03/demo03.js
+++ pages/demo03/demo03.js
@@ -1,20 +1,31 @@
 // pages/demo03/demo03.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    msg:'hello mina',
-    num:1000,
-    isGirl:false,
-    user:{
-      name:"张三",
-      age:25,
-      set:"男"
+    msg: "hello mina",
+    num: 1000,
+    isGirl: false,
+    user: {
+      name: "张三",
+      age: 25,
+      set: "男"
     },
-    isChecked:false
-  },
-
-  
-})
+    isChecked: false,
+    lists: [
+      {
+        id: 0,
+        name: "张三"
+      },
+      {
+        id: 1,
+        name: "李四"
+      },
+      {
+        id: 2,
+        name: "王五"
+      }
+    ]
+  }
+});
